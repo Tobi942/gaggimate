9b46994eac5166ee08aa9ef5e35f577f9e4e08a2
Tobi942 <to.lw@gmx.de>
2026-05-31 15:38:18 +0200

    Add version header file

---
+++ include/version.h
@@ -0,0 +1,8 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#define FIRMWARE_VERSION "2.0.0-flowcontroller"
+#define BUILD_GIT_VERSION "2.0.0"
+#define BUILD_TIMESTAMP __DATE__ " " __TIME__
+
+#endif // VERSION_H
